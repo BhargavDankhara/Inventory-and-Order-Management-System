39ecddd1439d291fa12209ca82605629d319b10c
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-10-17 00:21:01 +0530

    product CRUD

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -5,8 +5,15 @@ dotenv.config();
 export const ENV_VARS = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT || 5000,
+
   UPSTASH_REDIS_URL: process.env.UPSTASH_REDIS_URL,
+
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+
   NODE_ENV: process.env.NODE_ENV,
+
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
 import authRoute from "./routes/auth.route.js";
+import productRoute from "./routes/product.route.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
