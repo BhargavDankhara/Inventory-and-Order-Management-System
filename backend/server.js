947583a00121aa034818c18ab4e30870954515d3
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-10-17 07:34:28 +0530

    deployment ready!

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // Import environment variables
 import { ENV_VARS } from "./config/envVars.js";
@@ -15,6 +16,8 @@ import analyticsRoute from "./routes/analytics.route.js";
 const app = express();
 const PORT = ENV_VARS.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
@@ -25,6 +28,12 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoute);
 app.use("/api/analytics", analyticsRoute);
 
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
   connectDB();
